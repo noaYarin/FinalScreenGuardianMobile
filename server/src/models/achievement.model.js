c904b5e88ea2441192cbf723fde3640f90042657
Shiri667377 <sw667377@gmail.com>
2026-04-20 22:19:32 +0300

    update child schema for avatar and unlocked achievements

--- server/src/models/achievement.model.js
+++ server/src/models/achievement.model.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const AchievementSchema = new mongoose.Schema(
     {
-        title: { type: String, required: true },
+        key: { type: String, required: true, unique: true, trim: true },
+        title: { type: String },
         description: { type: String, required: true },
         icon: { type: String, default: "default.png" },
         xpReward: { type: Number, default: 0 },
--- server/src/models/avatar.schema.js
+++ server/src/models/avatar.schema.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
 export const AvatarSchema = new mongoose.Schema(
-    {
-        level: { type: Number, default: 1 },
-        img: { type: String, default: "default.png" },
-        currentXp: { type: Number, default: 0 },
-        nextLevelXp: { type: Number, default: 100 },
-    }, { timestamps: true }
+  {
+    level: { type: Number, default: 1, min: 1 },
+    img: { type: String, default: "default.png" },
+    currentXp: { type: Number, default: 0, min: 0 },
+    nextLevelXp: { type: Number, default: 100, min: 1 },
+  },
+  { _id: false }
 );
 
