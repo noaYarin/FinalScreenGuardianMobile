5278dfe59e89093d6e1f17aa02ddc404b45e0d1f
Noa <noaYarin066@gmail.com>
2026-04-23 23:24:39 +0300

    Remove ChooseChildAgeScreen component and related files, including styles and routing, to streamline the entering process.

--- csharp-api/Repositories/IPreLoginRecommendationRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ScreenGuardianAPI.Models;
-
-namespace ScreenGuardianAPI.Repositories
-{
-    public interface IPreLoginRecommendationRepository
-    {
-        PreLoginRecommendation? GetRecommendationByAge(int age);
-    }
-}
